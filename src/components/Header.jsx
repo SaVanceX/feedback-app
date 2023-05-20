ca447969aefc6b9a7af444d234a92b733116f382
SaVance Ford <savancef@gmail.com>
2023-05-20 04:28:08 -0500

    initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header(props) {
+  return (
+    <>
+      <header>
+        <div className='container'>
+          <h2>Feedback UI</h2>
+        </div>
+      </header>
+    </>
+  );
+}
