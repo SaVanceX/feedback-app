c0feb635d0da0a156bcd5b69f26d8d1c995a4e6a
SaVance Ford <savancef@gmail.com>
2024-12-25 00:09:57 -0600

    Managaing global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import React from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackLists from "./components/FeedbackLists";
 
 function App () {
+  const [feedback, setFeedback] = useState(FeedbackData)
 
   return(
     <>
     <Header />
     <div className="container">
-    <FeedbackItem />
+    <FeedbackLists feedback={feedback} />
 
 
     </div>
