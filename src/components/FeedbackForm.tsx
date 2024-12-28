131887740b2009c6b4ee0d427743aef15d4c8a36
SaVance Ford <savancef@gmail.com>
2024-12-28 14:20:47 -0600

    form validation

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -5,21 +5,36 @@ import Button from './Shared/Button'
 
 function FeedbackInput() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState<string | null>('')
+
+
 
   const handleTextChange = (e: any) => {
+    // todo refine form validation
     setText(e.target.value)
+
+    if(text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage('Text must be at least 10 characters')
+    } else {
+      setBtnDisabled(false)
+      setMessage(null)
+    }
   }
 
   return (
     <Card>
-      <form>
-        <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select component */}
-        <div className="input-group">
-          <input onChange={handleTextChange} type="text" placeholder='write a review' value={text} />
-          <Button type='submit' isDisabled={false}>Send</Button>
-        </div>
-      </form>
+      <h2>How would you rate your service with us?</h2>
+      {/* @todo - rating select component */}
+      <div className="input-group">
+        <input onChange={handleTextChange} type="text" placeholder='write a review' value={text} />
+        <Button type='submit' isDisabled={btnDisabled}>Send</Button>
+      </div>
+      {message && <div className='message'>{message}</div>}
     </Card>
   )
 }
